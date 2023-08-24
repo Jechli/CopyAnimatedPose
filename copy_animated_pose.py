78c9285e382d0089f58f88c8d0fc441139e50b8d
jechli <jenniferchingling@gmail.com>
2023-08-24 09:32:10 -0400

    Basic functionality finished

--- copy_animated_pose.py
+++ copy_animated_pose.py
@@ -47,7 +47,6 @@ class CopyPoseTool(QtWidgets.QDialog):
         self.copy_pose_layout.addWidget(self.copy_pose_list)
         self.copy_pose_layout.addWidget(self.copy_button)
         
-        
         # Section for selecting a static rig to paste to
         
         self.paste_to_label = QtWidgets.QLabel("Paste Pose To")
@@ -75,6 +74,7 @@ class CopyPoseTool(QtWidgets.QDialog):
         
 
     def create_connections(self):
+        
         self.copy_pose_list.itemClicked.connect(self.copy_rig_item_selected)
         self.paste_pose_list.itemClicked.connect(self.paste_rig_item_selected)
         self.copy_button.clicked.connect(self.copy_pose)
@@ -82,6 +82,7 @@ class CopyPoseTool(QtWidgets.QDialog):
         
 
     def update_lists(self):
+        
         list_of_rigs = [jnt for jnt in cmds.ls(type='joint') if not cmds.listRelatives(jnt, parent=True,type='joint')]
         self.copy_pose_list.clear()
         self.paste_pose_list.clear()
@@ -90,11 +91,46 @@ class CopyPoseTool(QtWidgets.QDialog):
             self.paste_pose_list.addItem(rig)
         
         
-        
     """
     Functionality
     """
     
+    def copy_rig_item_selected(self):   
+        selected_rig = self.copy_pose_list.currentItem().text()
+        cmds.select(selected_rig)
+        
+    
+    def paste_rig_item_selected(self):
+        
+        if len(cmds.ls(selection=True)) > 1:
+            self.copy_rig_item_selected()
+            
+        selected_rig = self.paste_pose_list.currentItem().text()
+        cmds.select(selected_rig, add=True)
+        
+    
+    def copy_pose(self):
+        
+        self.copied_rig_joint_names.clear()
+        self.copied_rig_translate.clear()
+        self.copied_rig_rotate.clear()
+        
+        root = self.copy_pose_list.currentItem().text()
+        self.add_joints_to_copy_list(root)
+        
+        print(self.copied_rig_joint_names)
+        print(self.copied_rig_translate)
+        print(self.copied_rig_rotate)
+        
+
+    def paste_pose(self):
+        
+        if (len(self.copied_rig_joint_names) and len(self.copied_rig_translate) and len(self.copied_rig_rotate)):
+            root_index = 0
+            root = self.paste_pose_list.currentItem().text()
+            self.copy_pose_to_rig(root_index, root)
+            
+        
     def add_joints_to_copy_list(self, root):
         
         root_translate = cmds.getAttr(root+'.translate')
@@ -109,33 +145,36 @@ class CopyPoseTool(QtWidgets.QDialog):
         if (children):
             for child in children:
                 self.add_joints_to_copy_list(child)
-                
 
-    # Save the rig joint information
-    def copy_rig_item_selected(self):   
-        self.copied_rig_joint_names.clear()
-        self.copied_rig_translate.clear()
-        self.copied_rig_rotate.clear()
-        
-        root = self.copy_pose_list.currentItem().text()
-        self.add_joints_to_copy_list(root)
-        
-        print(self.copied_rig_joint_names)
-        print(self.copied_rig_translate)
-        print(self.copied_rig_rotate)
-        
 
-    
-    # Paste copied rig joint info to selected static rig    
-    def paste_rig_item_selected(self):
-        pass
+    def copy_pose_to_rig(self, root_index, root):
         
-    
-    def copy_pose(self):
-        pass
+        translate_tuple = self.copied_rig_translate[root_index][0]
+        translate_X = translate_tuple[0]
+        translate_Y = translate_tuple[1]
+        translate_Z = translate_tuple[2]
         
-    def paste_pose(self):
-        pass
+        rotate_tuple = self.copied_rig_rotate[root_index][0]
+        rotate_X = rotate_tuple[0]
+        rotate_Y = rotate_tuple[1]
+        rotate_Z = rotate_tuple[2]
+        
+        cmds.setAttr(root+'.translateX', translate_X)
+        cmds.setAttr(root+'.translateY', translate_Y)
+        cmds.setAttr(root+'.translateZ', translate_Z)
+        cmds.setAttr(root+'.rotateX', rotate_X)
+        cmds.setAttr(root+'.rotateY', rotate_Y)
+        cmds.setAttr(root+'.rotateZ', rotate_Z)
+        
+        new_root_index = root_index + 1
+        
+        children = cmds.listConnections(root, type="joint", source=False)
+        
+        if (children):
+            for child in children:
+                new_root_index = self.copy_pose_to_rig(new_root_index, child)
+        
+        return new_root_index
 
 
 """
